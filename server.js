a0100289a94e5e5f353e48ae4c84fbcdde5a027d
Garrett Cox <garrettjcox@gmail.com>
2018-06-16 21:43:27 -0500

    Revert "Add video stream server"
    This reverts commit 13808635cff0663f916838c84598476b63740701.

--- server.js
+++ server.js
@@ -4,30 +4,16 @@ const kstatic = require('koa-static');
 const path = require('path');
 const router = require('./api/router');
 const auth = require('koa-basic-auth');
-const WebSocket = require('ws');
-const raspividStream = require('raspivid-stream');
 const rc = require('rc')('local', {
   port: 9000,
-  wsPort: 9001,
 });
 
 const server = new Koa();
-const wss = new WebSocket.Server({
-  port: rc.wsPort,
-});
 
 server
   .use(router.routes())
-  .use(kstatic(path.resolve(__dirname, 'public')));
+  .use(kstatic(path.resolve(__dirname, 'public')))
 
 server.listen(rc.port, () => {
   console.log(`Server listening on ${rc.port}`);
-});
-
-const stream = raspividStream();
-
-stream.on('data', (data) => {
-  ws.send(data, { binary: true }, (error) => {
-    if (error) console.error(error);
-  });
 });
